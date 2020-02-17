c97466a9837ee807cfe9c4b12a8f33965ff79665
小林　優誠 <kobayashi_yusei@waku-2.co.jp>
2020-02-17 16:42:34 +0900

    complete

--- src/App.js
+++ src/App.js
@@ -2,25 +2,28 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      count: 0,
+      message: 'initial'
+    }
+  }
+
+  render() {
+    return(
+      <>
+        <h2>{this.state.count}</h2>
+        <button onClick={() => this.setState({...this.state, count: this.state.count + 1})}>countUp</button>
+        <button onClick={() => this.setState({...this.state, count: this.state.count - 1})}>countDown</button>
+        <div>
+          <h2>{this.state.message}</h2>
+          <input type="text" value={this.state.message} onChange={(e) => this.setState({...this.state, message:e.target.value})} />
+        </div>
+      </>
+    )
+  }
 }
 
 export default App;
